ac1b838dafff4cba8aca0b9d8521bb4214be128f
corb20 <1397267259@qq.com>
2023-06-14 18:34:46 +0800

    little update

--- kernel/proc.c
+++ kernel/proc.c
@@ -124,6 +124,7 @@ allocproc(void)
 found:
   p->pid = allocpid();
   p->state = USED;
+  p->max_addr=TRAPFRAME;
 
   // Allocate a trapframe page.
   if((p->trapframe = (struct trapframe *)kalloc()) == 0){
